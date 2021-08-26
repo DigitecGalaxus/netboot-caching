7996e3bf91e5cc33ec06f63271a6c9048cd93da8
Thomas Gfeller <thomas.gfeller@digitecgalaxus.ch>
2021-08-26 14:21:45 +0200

    typo

--- fs/automounter.sh
+++ fs/automounter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-# Simply running two mount commands: The existing drives will be mounted and it's impossible two are available at the same time: NUCs have NVMEs (nvmeX), Proxmox-VMs have SSDs (vdaX).
+# Simply running three mount commands: The existing drives will be mounted and it's impossible two are available at the same time: NUCs have NVMEs (nvmeX), Proxmox-VMs have SSDs (vdaX/sdaX).
 mount -o defaults,nofail /dev/vda1 /home/master/netboot
 mount -o defaults,nofail /dev/sda1 /home/master/netboot
 mount -o defaults,nofail /dev/nvme0n1p1 /home/master/netboot
