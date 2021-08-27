a0b0f116946561060e79473324a573b4b5dcd3df
Thomas Gfeller <thomas.gfeller@digitecgalaxus.ch>
2021-08-27 07:24:31 +0200

    changing mountpath to match existing

--- fs/automounter.sh
+++ fs/automounter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 # Simply running three mount commands: The existing drive will be mounted and it's unlikely two are available at the same time: NUCs have NVMEs (nvmeX), Proxmox-VMs have SSDs (vdaX/sdaX).
-mount -o defaults,nofail /dev/vda1 /home/master/netboot
-mount -o defaults,nofail /dev/sda1 /home/master/netboot
-mount -o defaults,nofail /dev/nvme0n1p1 /home/master/netboot
+mount -o defaults,nofail /dev/vda1 /home/master/netboot/assets
+mount -o defaults,nofail /dev/sda1 /home/master/netboot/assets
+mount -o defaults,nofail /dev/nvme0n1p1 /home/master/netboot/assets
 
 # Create folders to avoid issues when syncing.
 mkdir -p /home/master/netboot/assets/prod /home/master/netboot/assets/dev /home/master/netboot/assets/kernels /home/master/netboot/assets/caching-server
