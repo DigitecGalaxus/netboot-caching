307e4ebab4bc0b7fb4af84dac5cd93277ad842ee
Thomas Gfeller <thomas.gfeller@digitecgalaxus.ch>
2021-08-27 10:51:04 +0200

    adjust automounting to support two partitions

--- fs/automounter.sh
+++ fs/automounter.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 # Simply running three mount commands: The existing drive will be mounted and it's unlikely two are available at the same time: NUCs have NVMEs (nvmeX), Proxmox-VMs have SSDs (vdaX/sdaX).
-mount -o defaults,nofail /dev/vda1 /home/master/netboot/assets
-mount -o defaults,nofail /dev/sda1 /home/master/netboot/assets
-mount -o defaults,nofail /dev/nvme0n1p1 /home/master/netboot/assets
+mount -o defaults,nofail /dev/vda2 /home/master/netboot/assets
+mount -o defaults,nofail /dev/sda2 /home/master/netboot/assets
+mount -o defaults,nofail /dev/nvme0n1p2 /home/master/netboot/assets
 
 # Create folders to avoid issues when syncing.
 mkdir -p /home/master/netboot/assets/prod /home/master/netboot/assets/dev /home/master/netboot/assets/kernels /home/master/netboot/assets/caching-server
+
+mount -o defaults,nofail /dev/vda1 /home/master/netboot/assets/caching-server
+mount -o defaults,nofail /dev/sda1 /home/master/netboot/assets/caching-server
+mount -o defaults,nofail /dev/nvme0n1p1 /home/master/netboot/assets/caching-server
+
+# Fixing permissions
 chown -R master.master /home/master/netboot/assets/
